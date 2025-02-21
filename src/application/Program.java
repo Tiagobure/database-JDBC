a7e5de0e1c1725b12702dc11b77b20de3c9b85c9
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-21 10:26:12 -0300

    fix: transacoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbException;
 import db.DbIntegrityException;
 
 public class Program {
@@ -13,27 +14,41 @@ public class Program {
 	public static void main(String[] args) {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 		try {
 			conn = DB.getConnection();
-			
-			st = conn.prepareStatement(
-					"DELETE FROM deparment "
-					+"WHERE "
-				    +"Id = ?");
-					st.setInt(1, 2);
-			
-			int rowsAffected = st.executeUpdate();
-			System.out.println("Done! Rows affected: " + rowsAffected);
-		}catch(SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		}finally {
+
+			conn.setAutoCommit(false);
+			st = conn.createStatement();
+
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+			//int x = 1;
+			//if (x < 2) {
+			//	throw new SQLException("Fake Error!");
+			//}
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+			conn.commit();
+			System.out.println("rows1 " + rows1);
+			System.out.println("rows2 " + rows2);
+
+		} catch (SQLException e) {
+			try {
+				
+				conn.rollback();
+				throw new DbException("Tramsaction rolled back! Caused by: "+ e.getMessage());
+				
+			} catch (SQLException e1) {
+
+				throw new DbException("Error trying to rollback! Caused by: "+ e1.getMessage());
+			}
+		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-		
+
 	}
 
 }
